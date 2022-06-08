94536b2d1ed76fd0e4ed580710b6e8e53134971f
Andrew Cutler <andrewkcutler@gmail.com>
2022-06-07 20:51:14 -0400

    highly necessary refactoring

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -1,27 +1,26 @@
 import { Box } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import {
-	Bar,
-	BarChart,
-	CartesianGrid,
-	Legend,
-	Tooltip,
-	XAxis,
-	YAxis
-} from 'recharts';
-import { IPlayerData, ISelectionPlayer, IStat } from './models/api.models';
-import { AppState, selectCheckedStats } from './store/slice';
+import { Bar, BarChart, CartesianGrid, Legend, XAxis, YAxis } from 'recharts';
+import { ISelectionPlayer } from './models/api.models';
 
 const COLORS = ['#6088aa', '#c09999', '#55a870'];
 
-// const CustomTooltip = (x: any): React.ReactElement => {
-// 	console.log(x);
-// 	if (x.payload && x.payload.length) {
-// 		return x.payload[0].name;
-// 	}
-// 	return <></>;
-// };
+const createKey = (stat: string, name: string): string =>
+	`${stat}_${name.replaceAll(/\s/g, '_')}`;
+
+interface IBarKey {
+	Key: string;
+	Name: string;
+}
+
+const getValueForStatByYear = (
+	player: ISelectionPlayer,
+	year: string,
+	stat: string
+): number | undefined =>
+	player.Data.find(({ Year }) => Year === year)?.Stats.find(
+		({ Name }) => Name === stat
+	)?.Value;
 
 // Need one chart for each stat, or Y-Axis will be skewed
 const Chart = ({
@@ -61,6 +60,7 @@ const Chart = ({
 			// 	}
 			// ];
 
+			// const nameToData = testData.reduce(
 			const nameToData = playerData.reduce(
 				(prev, curr) => ({ ...prev, [curr.Name]: curr }),
 				{} as { [key: string]: ISelectionPlayer }
@@ -77,24 +77,28 @@ const Chart = ({
 			const names = playerData.map(({ Name }) => Name);
 			// const names = testData.map(({ Name }) => Name);
 
-			// This is the worst thing ever.
-			const keys: any[] = [];
+			const uniqueKeys: IBarKey[] = [];
+
 			const _chartData = years
 				.map((year) => {
+					// interface { Year: string; [key in keyof Stats]: any }  (?)
 					const result: any = { Year: year };
 
-					const nameKeys = names.map((name) => [stat, name]);
-					for (const key of nameKeys) {
-						const _key = `${key[0]}_${key[1].replaceAll(
-							/\s/g,
-							'_'
-						)}`;
-						if (!keys.map(({ Key }) => Key).includes(_key)) {
-							keys.push({ Key: _key, Name: key[1] });
+					const nameKeys = names.map((name) => ({
+						Stat: stat,
+						Name: name
+					}));
+					for (const { Stat, Name } of nameKeys) {
+						const key = createKey(Stat, Name);
+						if (!uniqueKeys.map(({ Key }) => Key).includes(key)) {
+							uniqueKeys.push({ Key: key, Name });
 						}
-						result[_key] = nameToData[key[1]].Data.find(
-							({ Year }) => Year === year
-						)?.Stats.find(({ Name }) => Name === key[0])?.Value;
+
+						result[key] = getValueForStatByYear(
+							nameToData[Name],
+							year,
+							stat
+						);
 					}
 
 					return result;
@@ -103,15 +107,10 @@ const Chart = ({
 					YearA > YearB ? 1 : -1
 				);
 
-			console.log(_chartData);
 			setChartData(_chartData);
-			setChartKeys(Array.from(new Set(keys)));
+			setChartKeys(uniqueKeys);
 		}
 	}, [playerData, stat]);
-
-	useEffect(() => {
-		console.log(chartKeys);
-	}, [chartKeys]);
 
 	return (
 		<Box>
@@ -121,7 +120,7 @@ const Chart = ({
 					<XAxis dataKey='Year' />
 					<YAxis />
 					{/* <Tooltip content={<CustomTooltip />} /> */}
-					{/* <Legend /> */}
+					<Legend />
 					{chartKeys.map(({ Key, Name }, index) => (
 						<Bar
 							dataKey={Key}

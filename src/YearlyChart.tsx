a03eb60d78285b9a254401374b37141e422622ae
Andrew Cutler <Andrew.Cutler@fusionmgt.com>
2022-06-15 10:16:52 -0400

    fixed season in tooltip

--- src/YearlyChart.tsx
+++ src/YearlyChart.tsx
@@ -10,49 +10,37 @@ import {
 } from 'recharts';
 import { Box, Flex } from '@chakra-ui/react';
 import { RiZoomInLine, RiZoomOutLine } from 'react-icons/ri';
+import { buildYearlyChartData, getTooltipYear } from './utils/chart.yearly';
 import { useEffect, useState } from 'react';
 
 import { ISelectionPlayer } from './models/api.models';
 
-const COLORS = ['#6088aa', '#c09999', '#55a870'];
+const COLORS = ['#6088aa', '#c09999', '#55a870', '#89ddc0'];
 
 const CustomTooltip = ({
 	payload: tooltipPayload
 }: any): React.ReactElement => {
 	if (tooltipPayload && tooltipPayload.length > 0) {
-		console.log(tooltipPayload);
 		return (
 			<Flex
 				flexDirection='column'
 				color='white'
 				textShadow='1px 1px black'>
-				{tooltipPayload.map(({ payload, name, value }: any) => (
-					<div key={name}>
-						{name} ({payload.Year}
-						): {value}
-					</div>
-				))}
+				{tooltipPayload.map(
+					({ payload, name, value, dataKey }: any) => (
+						<div key={name}>
+							{name} ({getTooltipYear(payload, dataKey)}
+							): {value}
+						</div>
+					)
+				)}
 			</Flex>
 		);
 	}
+
 	return <></>;
 };
 
-const createKey = (stat: string, name: string): string =>
-	`${stat}_${name.replaceAll(/\s/g, '_')}`;
-
-interface IBarKey {
-	Key: string;
-	Name: string;
-}
-
-const getValueForStatByAge = (
-	player: ISelectionPlayer,
-	age: string,
-	stat: string
-): number | undefined => player.StatsByAge[age]?.Stats[stat];
-
-// Need one chart for each stat, or Y-Axis will be skewed
 const YearlyChart = ({
 	stat,
 	playerData
@@ -66,52 +54,10 @@ const YearlyChart = ({
 
 	useEffect(() => {
 		if (playerData && playerData.length > 0) {
-			const nameToData = playerData.reduce(
-				(prev, curr) => ({ ...prev, [curr.Name]: curr }),
-				{} as { [key: string]: ISelectionPlayer }
+			const { chartData: _chartData, uniqueKeys } = buildYearlyChartData(
+				stat,
+				playerData
 			);
-
-			// TODO: fix producing duplicates for ages both players share
-			// because it's not setting year/age pairs correctly
-			// step 1: new Set(all ages from both players)
-			// step 2: for each age, set year and data
-			const uniqueAges = Array.from(
-				new Set(
-					playerData
-						.map(({ StatsByAge }) => Object.keys(StatsByAge))
-						.flat()
-				)
-			);
-			console.log(uniqueAges);
-
-			const nameKeys = playerData.map(({ Name }) => ({
-				Stat: stat,
-				Name
-			}));
-
-			const uniqueKeys: IBarKey[] = [];
-
-			const _chartData = uniqueAges
-				.map((age) => {
-					// interface { Age: string; Year: string; [key in keyof Stats]: any }  (?)
-					const result: any = { Age: age };
-
-					for (const { Stat, Name } of nameKeys) {
-						const key = createKey(Stat, Name);
-						if (!uniqueKeys.map(({ Key }) => Key).includes(key)) {
-							uniqueKeys.push({ Key: key, Name });
-						}
-
-						result[key] = getValueForStatByAge(
-							nameToData[Name],
-							age,
-							stat
-						);
-					}
-
-					return result;
-				})
-				.sort(({ Age: AgeA }, { Age: AgeB }) => (AgeA > AgeB ? 1 : -1));
 
 			setChartData(_chartData);
 			setChartKeys(uniqueKeys);
---
+++ src/utils/chart.yearly.ts
@@ -0,0 +1,77 @@
+import { ISelectionPlayer } from '../models/api.models';
+import { v4 as uuidv4 } from 'uuid';
+
+const createKey = (stat: string, name: string): string =>
+	`${stat}_${name.replaceAll(/\s/g, '_')}`;
+
+interface IBarKey {
+	Key: string;
+	Name: string;
+}
+
+const getValueForStatByAge = (
+	player: ISelectionPlayer,
+	age: string,
+	stat: string
+): number | undefined => player.StatsByAge[age]?.Stats[stat];
+
+
+export const buildYearlyChartData = (stat: string, playerData: ISelectionPlayer[]) => {
+	const uniqueAges = Array.from(
+		new Set(
+			playerData
+				.map(({ StatsByAge }) => Object.keys(StatsByAge))
+				.flat()
+		)
+	);
+	const playerToId = playerData.reduce(
+		(dict, player) => ({ ...dict, [player.Name]: uuidv4() }),
+		{} as { [name: string]: string }
+	);
+
+
+	const uniqueKeys: IBarKey[] = [];
+
+	const chartData = uniqueAges
+		.map((age) => {
+			// interface { Age: string; Year: string; [key in keyof Stats]: any }  (?)
+			const result: any = { Age: age };
+			for (const player of playerData) {
+				if (player.StatsByAge[age]) {
+					const id = playerToId[player.Name];
+					const statKey = createKey(stat, id);
+					if (
+						!uniqueKeys.map(({ Key }) => Key).includes(statKey)
+					) {
+						uniqueKeys.push({
+							Key: statKey,
+							Name: player.Name
+						});
+					}
+
+					result[statKey] = getValueForStatByAge(
+						player,
+						age,
+						stat
+					);
+
+					const seasonKey = createKey('Season', id);
+					result[seasonKey] = player.StatsByAge[age].Year;
+				}
+			}
+
+			return result;
+		})
+		.sort(({ Age: AgeA }, { Age: AgeB }) => (AgeA > AgeB ? 1 : -1));
+
+	return { chartData, uniqueKeys };
+}
+
+export const getTooltipYear = (payload: any, dataKey: string) => {
+	const id = dataKey?.split('_')[1];
+	for (const key in payload) {
+		if (key === `Season_${id}`) {
+			return payload[key];
+		}
+	}
+}
